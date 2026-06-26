cfda9d5d65382ed8e05a269f3e251365d4e1d053
JonAsgeir <jon.torsvik@gmail.com>
2026-06-26 22:56:17 +0200

    Lagt til navigasjonsknapper på statistikkscenen

--- scripts/app_state.gd
+++ scripts/app_state.gd
@@ -4,3 +4,9 @@ var selected_subject_id: int = -1
 var selected_subject_name: String = ""
 var current_students: Array = []
 var current_student_index: int = 0
+
+func reset() -> void:
+	selected_subject_id = -1
+	selected_subject_name = ""
+	current_students.clear()
+	current_student_index = 0
--- scripts/statistics_page.gd
+++ scripts/statistics_page.gd
@@ -1,10 +1,17 @@
 extends Control
 
-@onready var subject_label: Label = $MarginContainer/ScrollContainer/VBoxContainer/HeaderLabel
-@onready var low_positive_interactions_label: Label = $MarginContainer/ScrollContainer/VBoxContainer/LowInteractionLabel
+@onready var subject_label: Label = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/HeaderLabel
+@onready var low_positive_interactions_label: Label = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/LowInteractionLabel
 
+@onready var back_button: Button = $MarginContainer/VBoxContainer/NavigationButtonsContainer/BackButton
+@onready var main_menu_button: Button = $MarginContainer/VBoxContainer/NavigationButtonsContainer/MainMenuButton
+@onready var quit_button: Button = $MarginContainer/VBoxContainer/NavigationButtonsContainer/QuitButton
 
 func _ready() -> void:
+	#SIGNALS
+	back_button.pressed.connect(_on_back_button_pressed)
+	main_menu_button.pressed.connect(_on_main_menu_button_pressed)
+	quit_button.pressed.connect(_on_quit_button_pressed)
 	#subject_label.text = "Fag: " + AppState.selected_subject_name
 	show_statistics()
 
@@ -26,3 +33,15 @@ func show_statistics() -> void:
 		]
 
 	low_positive_interactions_label.text = text
+	
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/student_page.tscn")
+
+
+func _on_main_menu_button_pressed() -> void:
+	AppState.reset()
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
